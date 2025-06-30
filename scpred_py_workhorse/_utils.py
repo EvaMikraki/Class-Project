ba773d15f49fbbeb166f6d0d56b2ed831a704ff5
Eva Mikraki Petroula <e.m.petroulal@gmail.com>
2025-06-30 20:37:19 +0300

    Updated directory.

--- scpred_py_workhorse/_utils.py
+++
@@ -1,8 +0,0 @@
-import scanpy as sc
-import anndata as ad
-
-def check_adata(adata):
-    """Checks if the input is an AnnData object."""
-    if not isinstance(adata, ad.AnnData):
-        raise TypeError("Input must be an AnnData object.")
-    print("AnnData object check passed.")
